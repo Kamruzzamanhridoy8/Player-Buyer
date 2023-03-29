8232c68809b478038b57c9dd564dd50cf5a158d0
Kamruzzamanhridoy8 <Kamruzzamanhridoy@hotmail.com>
2023-03-29 10:26:51 +0600

    finsih project

--- js/calling.js
+++ js/calling.js
@@ -1,5 +1,65 @@
-console.log('alu');
-let selectPlayer = document.querySelector('.player-select');
-selectPlayer.addEventListener('click',function(){
-    console.log('clicked');
-})
+// console.log('alu');
+// let selectPlayer = document.querySelector('.player-select');
+// selectPlayer.addEventListener('click',function(){
+//     console.log('clicked');
+// })
+
+function GetPlayerId(x){
+    
+    const totailLi = document.getElementsByTagName('li')
+    const numberofli = totailLi.length;
+    // console.log(numberofli);
+    if (numberofli >= 5) {
+        const disablebutton = document.getElementsByTagName('button')
+        disablebutton.disabled=true;
+        disablebutton.style.backgroundColor='gray'
+        console.log(numberofli);
+    }else{
+        x.disabled = true;
+        const id = x.id;
+        const DisColor = document.getElementById(id)
+        DisColor.style.backgroundColor='gray'
+
+        const ListOfPlayers = document.getElementById('List-of-players')
+        const CreatList = document.createElement('li')
+        CreatList.innerText=x.id;
+        CreatList.style.color='white'
+        ListOfPlayers.appendChild(CreatList)
+    }
+    
+}
+
+
+const AmountCal = ()=>{
+    const totailLi = document.getElementsByTagName('li')
+    const numberofli = totailLi.length;
+
+    const TotalAmountofPlayerfild = document.getElementById('per-player-amount-fild')
+    const getInputValue = TotalAmountofPlayerfild.value;
+    const totalPlayersExpanse = getInputValue*numberofli;
+    console.log(totalPlayersExpanse);
+
+    const DisplayPlayersAmmount = document.getElementById('ammount-of-players')
+    DisplayPlayersAmmount.innerText=totalPlayersExpanse;
+
+    return totalPlayersExpanse;
+
+}
+
+const TotalCalculation =()=>{
+    const TotalPlayersValue = AmountCal();
+
+    const AmountofMenagerfild = document.getElementById('Maneger-amount')
+    const getInputValueM = AmountofMenagerfild.value;
+
+    const AmountofCoachfild = document.getElementById('Coach-amount')
+    const getInputValueC = AmountofCoachfild.value;
+
+    const TotalGameBudget = TotalPlayersValue + parseInt(getInputValueM ) + parseInt(getInputValueC);
+    console.log(TotalGameBudget);
+
+    const DisplayGameAmmount = document.getElementById('display-total-ammount')
+    DisplayGameAmmount.innerText=TotalGameBudget;
+
+
+}
